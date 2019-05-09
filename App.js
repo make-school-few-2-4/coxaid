79e3d96957fba5b62be96d7fef96bdf2ccaeb2ab
Severiano Badajoz <sevebadajoz@gmail.com>
2019-05-09 14:42:38 -0700

    Fix syntax

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-import WorkoutList from 'WorkoutList';
-import WorkoutForm from 'WorkoutForm';
+import WorkoutList from './WorkoutList';
+import WorkoutForm from './WorkoutForm';
 
 
 const MainNavigator = createStackNavigator({
   WorkoutList: { screen: WorkoutList },
   WorkoutForm: { screen: WorkoutForm },
-})
+});
 
- export default createAppContainer(MainNavigator);
+export default createAppContainer(MainNavigator);
