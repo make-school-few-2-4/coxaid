df75df479f92e5d661b392c4f9e554b8ea884759
Severiano Badajoz <sevebadajoz@gmail.com>
2019-05-09 14:42:06 -0700

    Create project, create main navigator and boilerplating for components

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import WorkoutList from 'WorkoutList';
+import WorkoutForm from 'WorkoutForm';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const MainNavigator = createStackNavigator({
+  WorkoutList: { screen: WorkoutList },
+  WorkoutForm: { screen: WorkoutForm },
+})
+
+ export default createAppContainer(MainNavigator);
